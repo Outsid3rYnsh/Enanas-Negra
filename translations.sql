78d401ff10f6c6f784585f51cdec78d8ad4c2cf6
SaintYanLol <varga.yanosh.05@gmail.com>
2024-05-07 22:35:40 +0300

    -

--- translations.sql
+++
@@ -1,8 +0,0 @@
-USE TransSave;
-CREATE TABLE IF NOT EXISTS translations (
-    id INT PRIMARY KEY NOT NULL,
-    before_text TEXT,
-    after_text TEXT,
-    source_language VARCHAR(255),
-    target_language VARCHAR(255)
-);
